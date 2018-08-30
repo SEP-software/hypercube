87bf9be2b42394d11b70347c347d9cb8ede3ea5b
Robert Clapp <bob@DN525ima.SUNet>
2018-08-30 12:49:00 -0700

    added IOtypes

---
+++ lib/ioTypes.cc
@@ -0,0 +1,59 @@
+#include "ioTypes.h"
+#include <complex>
+#include<cassert>
+#include<string>
+using namespace SEP;
+dataType toElementType(const std::string &name) {
+  if (name == "BYTE") return DATA_BYTE;
+  if (name == "INT") return DATA_INT;
+  if (name == "FLOAT") return DATA_FLOAT;
+  if (name == "COMPLEX") return DATA_COMPLEX;
+  if (name == "DOUBLE") return DATA_DOUBLE;
+  return DATA_UNKNOWN;
+}
+
+std::string getTypeString(dataType typ){
+
+ switch(typ){
+   case DATA_BYTE:
+     return "BYTE";
+     break;
+   case DATA_FLOAT:
+     return "FLOAT";
+     break;
+   case DATA_COMPLEX:
+     return  "COMPLEX";
+     break;
+   case DATA_INT:
+     return  "INT";
+     break;
+   case DATA_DOUBLE:
+     return  "DOUBLE";
+     break;
+   default:
+     assert(1==2);
+}
+}
+size_t getDataTypeSize(dataType typ){
+
+ switch(typ){
+   case DATA_BYTE:
+     return sizeof(unsigned char);
+     break;
+   case DATA_FLOAT:
+     return sizeof(float);
+     break;
+   case DATA_COMPLEX:
+     return sizeof(std::complex< float>);
+     break;
+   case DATA_INT:
+     return sizeof(int);
+     break;
+   case DATA_DOUBLE:
+     return sizeof(double);
+     break;
+   default:
+     assert(1==2);
+}
+
+}
--- lib/ioTypes.h
+++ lib/ioTypes.h
@@ -1,5 +1,10 @@
+
+#include <string>
 namespace SEP{
-enum dataType { DATA_BYTE, DATA_INT, DATA_FLOAT, DATA_COMPLEX, DATA_DOUBLE };
+enum dataType { DATA_BYTE, DATA_INT, DATA_FLOAT, DATA_COMPLEX, DATA_DOUBLE,DATA_UNKNOWN };
+
+size_t getDataTypeSize(dataType typ);
+dataType toElementType(const std::string &name) ;
 
 
 
