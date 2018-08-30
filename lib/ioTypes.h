cdde29d11784b2100b07a26c913305d2e3efdbca
Robert Clapp <bob@roberts-mbp-3.lan>
2018-08-30 16:20:12 -0700

    pragma once

--- lib/ioTypes.h
+++ lib/ioTypes.h
@@ -1,12 +1,17 @@
-
+#pragma once
 #include <string>
-namespace SEP{
-enum dataType { DATA_BYTE, DATA_INT, DATA_FLOAT, DATA_COMPLEX, DATA_DOUBLE,DATA_UNKNOWN };
+namespace SEP {
+enum dataType {
+  DATA_BYTE,
+  DATA_INT,
+  DATA_FLOAT,
+  DATA_COMPLEX,
+  DATA_DOUBLE,
+  DATA_UNKNOWN
+};
 
 size_t getDataTypeSize(dataType typ);
-dataType toElementType(const std::string &name) ;
+dataType toElementType(const std::string &name);
 std::string getTypeString(dataType typ);
 
-
-
-}
+}  // namespace SEP
