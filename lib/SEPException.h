30eab7803a94d2f1b3ac1567ad2e8a68743d9cd4
Robert Clapp <bob@roberts-mbp-3.lan>
2019-01-27 16:21:51 -0800

    update submodule

--- lib/SEPException.h
+++ lib/SEPException.h
@@ -4,6 +4,7 @@ class SEPException : public std::exception {
 public:
     explicit SEPException(std::string  m) : message{m} {}
     virtual const char * what() const noexcept override {return message.c_str();}
+    std::string getMessage()const {return message;}
 private:
     std::string message = "";
 };
