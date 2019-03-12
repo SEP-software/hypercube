e9f722c241cc73b5e70d6dbf615800d7674cf74d
Robert Clapp <bob@roberts-mbp-3.lan>
2019-03-12 11:03:13 -0700

    update submodule

---
+++ lib/debug.cc
@@ -0,0 +1,30 @@
+#include "debug.h"
+#include "SEPException.h"
+using namespace SEP;
+void debug::addLine(std::stringstream &x) {
+  _lines.push_back(x.str());
+  if (_lines.size() >= _nflush) flush();
+}
+
+void errDebug::flush() {
+  for (int i = 0; i < _lines.size(); i++) std::cerr << _lines[i] << std::endl;
+  _lines.resize(0);
+}
+void fileDebug::flush() {
+  std::string x;
+  for (int i = 0; i < _lines.size(); i++) x = x + _lines[i] + "\n";
+  _file << x;
+  _file.flush();
+  _lines.resize(0);
+}
+void fileDebug::close() { _file.close(); }
+fileDebug::fileDebug(std::string &name, int nflush) {
+  _file.open(name);
+  if (!_file) SEPException("Trouble opening file");
+  setFlush(nflush);
+}
+
+std::vector<std::string> getDebug(std::string buffer) {
+  debugging *mydebug = &debugging::instance();
+  return mydebug->getDebug(buffer)->returnLines();
+}
---
+++ lib/debug.h
@@ -0,0 +1,56 @@
+#ifndef DEBUG_IO_H
+#define DEBUG_IO_H 1
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <sstream>  // std::stringstream
+#include <string>
+#include <vector>
+namespace SEP {
+class debug {
+ public:
+  void addLine(std::stringstream &x);
+  virtual void flush();
+  std::vector<std::string> returnLines() { return _lines; }
+  int setFlush(int nf) { _nflush = nf; }
+  virtual void close() { ; }
+  std::vector<std::string> _lines;
+
+ private:
+  int _nflush;
+};
+class errDebug : public debug {
+ public:
+  errDebug(int nflush = 1000000) { setFlush(nflush); }
+  virtual void flush();
+};
+class fileDebug : public debug {
+ public:
+  fileDebug(std::string &name, int nflush = 1000000);
+  virtual void close();
+  virtual void flush();
+
+ private:
+  std::ofstream _file;
+};
+class debugging {
+ public:
+  static debugging &instance() {
+    static debugging instance;
+    return instance;
+  }
+  void addDebug(std::string name, std::shared_ptr<debug> d) {
+    _debugs[name] = d;
+  }
+  std::shared_ptr<debug> getDebug(std::string &name);
+
+ private:
+  debugging() { ; }
+  ~debugging() { ; }
+  std::map<std::string, std::shared_ptr<debug>> _debugs;
+};
+
+}  // namespace SEP
+std::vector<std::string> getDebug(std::string buffer);
+#endif
