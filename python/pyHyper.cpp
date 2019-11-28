08fa1d4f8e469f29eb171830641052662ff4c8bc
Bob Clapp <bob@sep.stanford.edu>
2019-11-28 03:16:12 +0000

    bug[A

--- python/pyHyper.cpp
+++ python/pyHyper.cpp
@@ -61,7 +61,7 @@ PYBIND11_MODULE(pyHypercube, clsHyper) {
       .def("sameSize",
            (bool (hypercube::*)(const std::shared_ptr<hypercube> &) const) &
                hypercube::sameSize,
-           "Check to see if hypercube is the same size");
+           "Check to see if hypercube is the same size")
       .def("checkSame",
            (bool (hypercube::*)(const std::shared_ptr<hypercube> &) const) &
                hypercube::checkSame,
