ebf548f080ce231f49bd41683ae418d2088427ef
Bob Clapp <bob@sep.stanford.edu>
2019-12-10 06:03:46 +0000

    docker builds

--- python/pyDebug.cpp
+++ python/pyDebug.cpp
@@ -3,16 +3,6 @@
 #include "debug.h"
 namespace py = pybind11;
 namespace SEP {
-PYBIND11_MODULE(SEPException, m) {
-  static py::exception<SEPException> exc(m, "SEPError");
-  py::register_exception_translator([](std::exception_ptr p) {
-    try {
-      if (p) std::rethrow_exception(p);
-    } catch (const SEPException &e) {
-      exc(e.what());
-    }
-  });
-}
 PYBIND11_MODULE(pyDebugging, m) {
   m.def("getDebug", &getDebug, "Return debugging information");
 }
